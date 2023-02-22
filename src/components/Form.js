71871513cd8706d5317fdcf2f3cbadc44e5af3fc
jd richards <jdrichardstech@gmail.com>
2023-02-22 11:30:28 -0500

    initial commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,29 +1,20 @@
-import { useState } from "react";
-//This is how I import and consume the context if I want to use the custom hook from LanguageContext
-import { useLanguage } from "../context/LanguageContext";
+import { useState } from 'react';
 
-//the NavBar which is singly nested in the App.js can consume the context. Meanwhile so can this deeply next Form.js component
 export default function Form() {
-  // destructure and consume only the data, state or functions that I need from the component
-  const { language, setLanguage, english, french } = useLanguage();
-
-  const [name, setName] = useState({ firstName: "", lastName: "" });
+  const [name, setName] = useState({ firstName: '', lastName: '' });
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    // no need to make this work. out of scope of project
   };
-
-  const { greeting, instruction, select, firstName, lastName, button } =
-    language === "english" ? english : french;
 
   return (
     <>
-      <h1>{greeting} 😃!</h1>
+      <h1>Hello! 😃!</h1>
       <h2>
-        {select} <span onClick={() => setLanguage("french")}>FR</span> |
-        <span onClick={() => setLanguage("english")}>EN</span>
+        Choose Your Language <span>FR</span> |<span>EN</span>
       </h2>
-      <p>{instruction}</p>
+      <p>Please Fill Out This Form</p>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -39,7 +30,7 @@ export default function Form() {
           id={name.lastName}
           onChange={(e) => setName({ ...name, [e.target.id]: e.target.value })}
         />
-        <button type="submit">{button}</button>
+        <button type="submit">Submit</button>
       </form>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { LanguageProvider } from "./context/LanguageContext";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-// Wrap the whole application with the LanguageContext Provider
-const Main = () => {
-  return (
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
-  );
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Main />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
